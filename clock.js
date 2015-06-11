2bfebeff27c5bfc6ab7bf2a1c79a3c46504fb5f2
Calvin Webster <calweb@gmail.com>
2015-06-11 11:13:17 -0400

    added more stuff

--- clock.js
+++ clock.js
@@ -9,4 +9,16 @@
   var seconds = dt.getSeconds();
   console.log(hours, minutes, seconds);
 
+  var minutes = minutes.toString().length > 1 ? minutes :  "0" + minutes;
+
+  var seconds = seconds.toString().length > 1 ? seconds :  "0" + seconds;
+
+
+  $(function () {
+    var $time = $('#time');
+    $time.find('.hrs').text(hours + ":");
+    $time.find('.mins').text(minutes + ":");
+    $time.find('.secs').text(seconds);
+  });
+
 })();
