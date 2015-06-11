10be843c3e3ee58f223fc76e4086062d715351f5
Calvin Webster <calweb@gmail.com>
2015-06-11 11:33:31 -0400

    functional clock and bg colors ftw!!!

--- clock.js
+++ clock.js
@@ -12,13 +12,33 @@
   var minutes = minutes.toString().length > 1 ? minutes :  "0" + minutes;
 
   var seconds = seconds.toString().length > 1 ? seconds :  "0" + seconds;
-
-
   $(function () {
     var $time = $('#time');
+
     $time.find('.hrs').text(hours + ":");
     $time.find('.mins').text(minutes + ":");
     $time.find('.secs').text(seconds);
+
+
+
+  setInterval(function() {
+    console.log("its working");
+    var dt = new Date();
+    var hours = dt.getHours();
+    var minutes = dt.getMinutes();
+    var seconds = dt.getSeconds();
+    var minutes = minutes.toString().length > 1 ? minutes :  "0" + minutes;
+
+    var seconds = seconds.toString().length > 1 ? seconds :  "0" + seconds;
+    $time.find('.hrs').text(hours + ":");
+    $time.find('.mins').text(minutes + ":");
+    $time.find('.secs').text(seconds);
+    var bgColor = randomHex();
+    console.log(bgColor);
+    $("body").css("background-color", "#" + bgColor);
+  }, 1000);
   });
+
+
 
 })();
